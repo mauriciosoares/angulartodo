2c09ada67174aab9eb5622e91db7196ae24a3e01
Mauricio Soares <m.soares2603@gmail.com>
2013-08-14 02:26:23 -0300

    add search engine, separate current from archived todos

--- js/todo.js
+++ js/todo.js
@@ -6,10 +6,11 @@ function TodoCtrl($scope) {
 		{text: 'build an angular app', done: false}
 	];
 
+	$scope.arcTodos = [];
+
 	$scope.addTodo = function() {
 		$scope.todos.push({text: $scope.todoText});
 		$scope.todoText = '';
-		console.log('teste');
 	};
 
 	$scope.remaining = function() {
@@ -20,5 +21,28 @@ function TodoCtrl($scope) {
 			}
 		});
 		return count;
+	};
+
+	$scope.allArcTodos = function() {
+		return $scope.arcTodos.length;
 	}
+
+	$scope.archive = function() {
+		var oldTodos = $scope.todos;
+		$scope.todos = [];
+
+		angular.forEach(oldTodos, function(todo) {
+			if(!todo.done) {
+				$scope.todos.push(todo);
+			}else{
+				$scope.arcTodos.push(todo);
+			}
+		});
+	}
+
+	$scope.updateTodo = function(todo, newText) {
+		console.log($scope.todos.length);
+		todo.text = newText;
+	};
+
 }
